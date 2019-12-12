e6385733a824ddf0b4cc70794616c35039697218
Henry Suzukawa <henry.suzukawa@telecom-paris.fr>
2019-12-12 14:39:56 +0100

    Fix performances issues.

--- wishbone/controleur_memoire/wb_bram/wb_bram.sv
+++ wishbone/controleur_memoire/wb_bram/wb_bram.sv
@@ -11,56 +11,38 @@ module wb_bram #(parameter mem_adr_width = 11) (
       );
       // a vous de jouer a partir d'ici
 
-      logic [mem_adr_width-1:0] mem_addr;
-//      logic [31:0] mask;
-      logic [31:0] mem [2**(mem_adr_width)-1:0];
-//      logic [7:0] mem1 [2**mem_adr_width-1:0];
-//      logic [7:0] mem2 [2**mem_adr_width-1:0];
-//      logic [7:0] mem3 [2**mem_adr_width-1:0];
-
-      logic ack_w, ack_r;
-
+      // ----------------------- MEMORY ---------------------------
+      logic [3:0][7:0] mem [2**(mem_adr_width)-1:0];
+      wire [mem_adr_width-1:0] mem_addr;
       assign mem_addr = wb_s.adr[mem_adr_width+1:2];
+
+      // ------------------------ ACK SIGNAL ----------------------
+      logic ack_w, ack_r;
       assign wb_s.ack = (ack_w || ack_r);
-      assign act_b = wb_s.cyc & wb_s.stb;
-      assign ack_w = (act_b & wb_s.we);
-      // assign mask[7:0] = {8{wb_s.sel[0]}};
-      // assign mask[15:8] = {8{wb_s.sel[1]}};
-      // assign mask[23:16] = {8{wb_s.sel[2]}};
-      // assign mask[31:24] = {8{wb_s.sel[3]}};
+      assign ack_w = (wb_s.cyc & wb_s.stb & wb_s.we);
 
+      // Pour cette partie, on ne utilise pas les signaux rty et err
+      assign wb_s.rty = 0;
+      assign wb_s.err = 0;
+
+      // ------------------------ RST et ACK ----------------------
       always_ff @(posedge wb_s.clk) begin
-            ack_r <= 0;
-            if(wb_s.rst) begin
-                  wb_s.rty <= 0;
-                  wb_s.err <= 0;
-
+            if(wb_s.rst | ack_r) begin
                   ack_r <= 0;
             end
-            else if (act_b && !wb_s.we)
-                  ack_r <= 1'b1;
+            else if (wb_s.stb & !wb_s.we)
+                  ack_r <= 1;
       end
 
+      // ------------------------ READ et WRITE -------------------
       always_ff @(posedge wb_s.clk) begin
             if(ack_w) begin
-                  if(wb_s.sel[0])
-                        mem[mem_addr][7:0] <= wb_s.dat_ms[7:0];
-                  if(wb_s.sel[1])
-                        mem[mem_addr][15:8] <= wb_s.dat_ms[15:8];
-                  if(wb_s.sel[2])
-                        mem[mem_addr][23:16] <= wb_s.dat_ms[23:16];
-                  if(wb_s.sel[3])
-                        mem[mem_addr][31:24] <= wb_s.dat_ms[31:24];
-                  //  mem[mem_addr] <= (mem[mem_addr] & ~mask) | (wb_s.dat_ms & mask); 
+                  if(wb_s.sel[0]) mem[mem_addr][0] <= wb_s.dat_ms[7:0];
+                  if(wb_s.sel[1]) mem[mem_addr][1] <= wb_s.dat_ms[15:8];
+                  if(wb_s.sel[2]) mem[mem_addr][2] <= wb_s.dat_ms[23:16];
+                  if(wb_s.sel[3]) mem[mem_addr][3] <= wb_s.dat_ms[31:24];
             end
+            wb_s.dat_sm <= mem[mem_addr];
       end
-
-      always_ff @(negedge wb_s.clk) begin
-            if(ack_r) begin
-//                  wb_s.dat_sm <= {mem[mem_addr+3], mem[mem_addr+2], mem[mem_addr+1], mem[mem_addr]};
-                  wb_s.dat_sm <= mem[mem_addr];
-            end
-      end
-
 
       endmodule
